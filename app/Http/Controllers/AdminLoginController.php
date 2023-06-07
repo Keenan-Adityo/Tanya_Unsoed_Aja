404f2e64ee4b86069a6ecda717b623ca63b949ed
Egoenk <neoegoenk@gmail.com>
2023-06-07 22:41:43 +0700

    Update Login Controler Kurang Session

--- app/Http/Controllers/AdminLoginController.php
+++ app/Http/Controllers/AdminLoginController.php
@@ -4,41 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\RedirectResponse;
 use App\Models\Admin;
 
-class AdminLoginController extends Controller
-{
-
-    public function index()
-    {
-        return view('login');
-    }
+class AdminLoginController extends Controller{
 
     public function login(Request $request)
     {
-        $credentials = $request->validate([
-            'username' => 'required',
-            'password' => 'required'
-        ]);
+        $username = $request->input('username');
+        $password = $request->input('password');
+        $data = Admin::where('username', $username)->get();
 
-        if(Auth::attempt($credentials))
-        {
-            $request->session()->regenerate();
-            return redirect()->route('dashboard')
-                ->withSuccess('You have successfully logged in!');
+        if ($data && $data->isNotEmpty() && $username === $data->first()->username && $password === $data->first()->password) {
+            return redirect('/test');
+        }else{
+            return redirect()->back();
         }
-
-        return back()->withErrors([
-            'email' => 'Your provided credentials do not match in our records.',
-        ])->onlyInput('email');
-    }
-
-
-    public function logout(Request $request)
-    {
-        Auth::guard('admin')->logout();
-
-        return redirect('/loginAdmin');
     }
 }
